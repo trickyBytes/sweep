1b8a474fdad5a3f538ef482b3f57a0693a9a524d
trickyBytes <richard.tiffin@akoolla.com>
2017-11-29 11:26:02 +0000

    Added full integration test

--- src/test/java/sweep/impl/TillTest.java
+++ src/test/java/sweep/impl/TillTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -15,9 +16,9 @@ import org.junit.Test;
 import sweep.IBasket;
 import sweep.ISaving;
 import sweep.ITill;
-import sweep.impl.Basket;
-import sweep.impl.Till;
 import sweep.offers.IOffer;
+import sweep.offers.impl.BulkPriceOffer;
+import sweep.offers.impl.GetNthItemFreeOffer;
 import sweep.products.IProduct;
 import sweep.products.impl.Beans;
 import sweep.products.impl.Coke;
@@ -41,7 +42,7 @@ public class TillTest {
     public void setUp(){
         coke = new Coke(new Price(70));
         beans = new Beans(new Price(50));
-        oranges = new Oranges(new Price(0.00199));
+        oranges = new Oranges(new Price(0.199));//price per gram
     }
         
     @Test
@@ -91,5 +92,25 @@ public class TillTest {
         assertTrue("Subtotal", new BigDecimal(190).equals(((Till)till).calculateSubTotal(basket)));
         assertTrue("Savings", new BigDecimal(-40).equals(((Till)till).calculate(basket, Arrays.asList(offer))));
         assertTrue("Total Price", new BigDecimal(150).equals(till.calculateTotal(basket)));
+    }
+    
+    @Test
+    public void fullTillIntegrationTest() throws Exception {
+        IOffer beansOffer = new GetNthItemFreeOffer(beans.getId(), 2);
+        IOffer cokeOffer = new BulkPriceOffer(coke.getId(), 2, new BigDecimal(100));
+        
+        till = new Till();
+        till.addOffer(beansOffer);
+        till.addOffer(cokeOffer);
+
+        basket = new Basket();
+        basket.addProduce(coke, 2);
+        basket.addProduce(beans, 3);
+        basket.addProduce(oranges, 200);
+        
+        System.out.println(((Till)till).calculateSubTotal(basket).toString());
+        assertEquals("Subtotal", new BigDecimal(330).doubleValue(), ((Till)till).calculateSubTotal(basket).doubleValue(), 0.2d);
+        assertEquals("Savings", new BigDecimal(-90).doubleValue(), ((Till)till).calculate(basket, Arrays.asList(beansOffer, cokeOffer)).doubleValue(), 0.2d);
+        assertEquals("Total Proce", new BigDecimal(240).doubleValue(), till.calculateTotal(basket).doubleValue(), 0.2d);
     }
 }
